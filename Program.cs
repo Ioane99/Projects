80069ce2be9a68433a7beaddf08ea3edadd82c74
Ioane99 <142022160+Ioane99@users.noreply.github.com>
2024-06-29 12:51:58 +0400

    Add files via upload
    Number guessing game

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyfirstProgram
+{
+    class Program
+    {
+        static void Main(String[] args)
+        { // es komentaria btw 
+            Random random = new Random();
+            bool playagain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int guesses;
+            int number;
+            String response;
+
+            while (playagain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next( min, max + 1);
+                  while(guess != number)
+                {
+                    Console.WriteLine("Guess the number between " + min + " - " + max );
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess:" + guess);
+                     if (guess > number)
+                    {
+                        Console.WriteLine(guess + " is too high");
+                    }
+                    if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is too low");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("You Guessed Right Number!");
+                Console.WriteLine("Would you like to play again? (Y/N) Y = yes, N = No");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+                 if (response == "Y")
+                {
+                    playagain = true;
+
+                }
+                else 
+                {
+                    playagain = false;
+
+                }
+
+
+            }
+          
+
+            
+        }
+    }
+}
